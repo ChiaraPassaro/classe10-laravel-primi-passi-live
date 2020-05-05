70be1f471539738ab145c5c7fa70ed2186aecf7d
Luigi Fontana <luigifontana92@gmail.com>
2020-05-05 12:10:02 +0200

    Test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,25 +1,12 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-     <head>
-          <meta charset="utf-8">
-          <title>Home</title>
-     </head>
-     <body>
-          <header>
-               <nav>
-                    <ul>
-                         <li>
-                              <a href="#">Home</a>
-                         </li>
-                         <li>
-                              <a href="{{route('prodotti')}}">Prodotti</a>
-                         </li>
-                         <li>
-                              <a href="#">Contatti</a>
-                         </li>
-
-                    </ul>
-               </nav>
-          </header>
-     </body>
+    <head>
+        <meta charset="utf-8">
+        <title>Home</title>
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    </head>
+    <body>
+        @include ('partials.header')
+        @include ('partials.footer')
+    </body>
 </html>
